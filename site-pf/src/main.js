0d2a678f4ef18b54bc607aeccc83c9df86fa3c8e
Bruna <bborgesfreire@gmail.com>
2025-04-02 22:15:47 -0300

    Adicionando breakpoints

--- site-pf/src/main.js
+++ site-pf/src/main.js
@@ -1,6 +1,27 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import theme from "@/utils/theme"; 
 
-import { createApp } from 'vue'
-import App from './App.vue'
+const app = createApp(App);
 
-createApp(App).mount('#app')
+app.config.globalProperties.$theme = theme;
+
+const root = document.documentElement;
+
+Object.entries(theme.colors).forEach(([key, value]) => {
+  root.style.setProperty(`--color-${key}`, value);
+});
+
+Object.entries(theme.fonts.sizes).forEach(([key, value]) => {
+  root.style.setProperty(`--font-size-${key}`, value);
+});
+
+Object.entries(theme.fonts.family).forEach(([key, value]) => {
+  root.style.setProperty(`--font-${key}`, value);
+});
+
+Object.entries(theme.fonts.weight).forEach(([key, value]) => {
+  root.style.setProperty(`--font-weight-${key}`, value);
+});
+
+app.mount("#app");
