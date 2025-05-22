6209cc6506f9e0fcaa990be2560091b5261c12dd
BruBF <bborgesfreire@gmail.com>
2025-05-22 18:35:49 -0300

    header, falta centralizar o conteudo

--- site-pf/src/App.vue
+++ site-pf/src/App.vue
@@ -18,10 +18,20 @@ import  Footer  from './components/Footer.vue'
 </template>
 
 <style scoped>
+/* src/assets/main.css */
+@import url('https://fonts.googleapis.com/css2?family=Island+Moments&display=swap');
+
 header {
   line-height: 1.5;
 }
 
+.logo-text {
+  color: var(--background);
+  font-family: "Island Moments", cursive;
+  font-weight: 400;
+  font-size: 30px;
+  letter-spacing: 1px;
+}
 .logo {
   display: block;
   margin: 0 auto 2rem;
